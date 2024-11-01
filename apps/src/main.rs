60959033ebd49c6ba12a6f86d3e3f3b6753c7571
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-11-02 04:29:57 +0700

    local proof problem & clarify receipt

--- apps/src/main.rs
+++ apps/src/main.rs
@@ -2,8 +2,9 @@ mod utils{
     pub mod generating_proof;
     pub mod lib ; 
 }
+use ethers::core::k256::U256;
 use utils::generating_proof::{generating_proof} ;
-use utils::lib::{convertImageToU8} ; 
+use utils::lib::{convertImageToU8,print_receipt_properties} ; 
 
 use methods::FIBONACCI_GUEST_ID;
 
@@ -32,27 +33,35 @@ async fn main(){
     // wanted to publish generated receipt to sepolia
     // initialize chain connect cofiguration
     dotenv().ok();
-    let rpc_url : String = format!("https://{}.g.alchemy.com/v2/{}",
-        env::var("NETWORK").unwrap().as_str(),
-        env::var("ALCHEMY_API_KEY").unwrap().as_str()
-    );
+    // let rpc_url : String = format!("https://{}.g.alchemy.com/v2/{}",
+    //     env::var("NETWORK").unwrap().as_str(),
+    //     env::var("ALCHEMY_API_KEY").unwrap().as_str()
+    // );
 
     // Set up providers and user's wallet 
-    let provider = Provider::<Http>::try_from(rpc_url.as_str()).expect("Failed to create provider"); 
-    let private_key = env::var("PRIVATE_KEY").expect("PRIVATE_KEY must be set");
-    let wallet : LocalWallet = private_key.parse().expect("invalid private key");
-    let client = Arc::new(SignerMiddleware::new(provider.clone(), wallet.clone()));
+    // let provider = Provider::<Http>::try_from(rpc_url.as_str()).expect("Failed to create provider"); 
+    // let private_key = env::var("PRIVATE_KEY").expect("PRIVATE_KEY must be set");
+    // let wallet : LocalWallet = private_key.parse().expect("invalid private key");
+    // let client = Arc::new(SignerMiddleware::new(provider.clone(), wallet.clone()));
     // let signer = SignerMiddleware::new(provider.clone(), wallet.clone());
 
-    let verifier_contract_address : Address = "0xf70aBAb028Eb6F4100A24B203E113D94E87DE93C".parse().unwrap() ; 
-    let verifier_contract = RiscZeroVerifierRouter::new(verifier_contract_address, client.clone()) ; 
+    // let verifier_contract_address : Address = "0xf70aBAb028Eb6F4100A24B203E113D94E87DE93C".parse().unwrap() ; 
+    // let verifier_contract = RiscZeroVerifierRouter::new(verifier_contract_address, client.clone()) ; 
 
     // Try using import generating receipt function from another folder
-    let (seal, journal) = generating_proof().expect("Failed to generate proof data");
-    let image_id = convertImageToU8(FIBONACCI_GUEST_ID) ; 
-    let journal_digest = Sha256::digest(journal.clone()) ; 
-    
-    let binding = verifier_contract.verify(seal.into(), image_id.into() , journal_digest.into());
-    let transaction = binding.send().await.expect("Transaction failed");
-    println!("Transaction hash: {:?}", transaction);
+    // let (seal, journal) = 
+    let receipt = generating_proof().expect("Failed to generating receipt") ;// .expect("Failed to generate proof data");
+    print_receipt_properties(&receipt) ;
+
+    // let image_id = convertImageToU8(FIBONACCI_GUEST_ID) ; 
+    // let journal_digest = Sha256::digest(journal.clone()) ; 
+    // // let x = U256::abi_decode(&journal, true).context("decoding journal data")?;
+
+    // println!("Seal: {:?}", seal);
+    // println!("Image ID: {:?}", image_id);
+    // println!("Journal Digest: {:?}", journal_digest);
+
+    // let binding = verifier_contract.verify(seal.into(), image_id.into() , journal_digest.into());
+    // let transaction = binding.send().await.expect("Transaction failed");
+    // println!("Transaction hash: {:?}", transaction);
 }
--- apps/src/utils/lib.rs
+++ apps/src/utils/lib.rs
@@ -1,3 +1,8 @@
+use anyhow::{bail, Result};
+use risc0_zkvm::{sha::Digestible, Receipt};
+
+
+
 pub fn convertImageToU8(vector32: [u32;8]) -> [u8;32] {
     let mut result : [u8;32] = [0u8;32] ; 
 
@@ -6,7 +11,50 @@ pub fn convertImageToU8(vector32: [u32;8]) -> [u8;32] {
         let start = i * 4;
         result[start..start + 4].copy_from_slice(&bytes);
     }
-
-
     result
+}
+
+// pub fn encode_seal_lib(receipt: &risc0_zkvm::Receipt) -> Result<Vec<u8>> {
+//     println!("Receipt {:?}",receipt) ;
+//     let seal = match receipt.inner.clone() {
+//         InnerReceipt::Fake(receipt) => {
+//             let seal = receipt.claim.digest().as_bytes().to_vec();
+//             let selector = &[0u8; 4];
+//             // Create a new vector with the capacity to hold both selector and seal
+//             let mut selector_seal = Vec::with_capacity(selector.len() + seal.len());
+//             selector_seal.extend_from_slice(selector);
+//             selector_seal.extend_from_slice(&seal);
+//             selector_seal
+//         }
+//         InnerReceipt::Groth16(receipt) => {
+//             let selector = &receipt.verifier_parameters.as_bytes()[..4];
+//             // Create a new vector with the capacity to hold both selector and seal
+//             let mut selector_seal = Vec::with_capacity(selector.len() + receipt.seal.len());
+//             selector_seal.extend_from_slice(selector);
+//             selector_seal.extend_from_slice(receipt.seal.as_ref());
+//             selector_seal
+//         }
+//         _ => bail!("Unsupported receipt type"),
+//     };
+//     Ok(seal)
+// }
+
+
+pub fn print_receipt_properties(receipt: &Receipt) {
+    // Print the full receipt for debugging
+    // println!("Receipt claim : {:?}", receipt.claim());
+    println!("Receipt inner then claim : {:?}",receipt.inner.claim()) ;
+    
+    let public_output : u64 = receipt.journal.decode().unwrap() ; 
+    println!("Public output : {:?}",public_output) ;
+
+    // Digest is using shar256 hash 
+    println!("Receipt : journal : {:?}",receipt.journal.bytes.clone()) ;
+    println!("Receipt : journal digest as byte : {:?}",receipt.journal.bytes.clone().digest().as_bytes() ) ;
+    println!("Receipt : metadata : {:?}",receipt.metadata.clone().verifier_parameters.as_bytes()) ;
+    let seal = receipt.claim().unwrap().digest() ; 
+    println!("Seal : {:?}",seal) ;
+    println!("Seal as bytes : {:?}",seal.as_bytes()) ;
+    println!("Seal as bytes to vec : {:?}",seal.as_bytes().to_vec()) ;
+
 }
