04a69cf68cfebd68ecc742275acb7a7189619b56
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-11-06 01:40:46 +0700

    refactor call verify proof onchain

--- apps/src/main.rs
+++ apps/src/main.rs
@@ -3,15 +3,18 @@ mod utils {
     pub mod lib;
 }
 
+use risc0_zkvm::{sha::Digestible};
+// use risc0_ethereum_contracts::encode_seal;
 use utils::generating_proof::generating_proof;
-use utils::lib::{print_receipt_properties , encode_seal_lib};
-
-use risc0_ethereum_contracts::encode_seal;
+// use utils::lib::{print_receipt_properties };
 
 use dotenv::dotenv;
+
 use std::env;
 use std::sync::Arc;
-use ethers::prelude::{abigen, Address, Http, LocalWallet, Provider, SignerMiddleware};
+
+
+use ethers::prelude::{abigen, Address, Http, LocalWallet, Provider, SignerMiddleware , Signer, Middleware, TransactionRequest };
 
 abigen!(
     FibonacciVerifier,
@@ -23,34 +26,45 @@ async fn main() {
     // wanted to publish generated receipt to sepolia
     // initialize chain connect cofiguration
     dotenv().ok();
-    // let rpc_url: String = format!(
-    //     "https://{}.g.alchemy.com/v2/{}",
-    //     env::var("NETWORK").unwrap().as_str(),
-    //     env::var("ALCHEMY_API_KEY").unwrap().as_str()
-    // );
+    let rpc_url: String = format!(
+        "https://{}.g.alchemy.com/v2/{}",
+        env::var("NETWORK").unwrap().as_str(),
+        env::var("ALCHEMY_API_KEY").unwrap().as_str()
+    );
+    let chain_id = env::var("CHAIN_ID").unwrap().parse::<u64>().unwrap(); 
 
-    // // Set up providers and user's wallet
-    // let provider = Provider::<Http>::try_from(rpc_url.as_str()).expect("Failed to create provider");
-    // let private_key = env::var("PRIVATE_KEY").expect("PRIVATE_KEY must be set");
-    // let wallet: LocalWallet = private_key.parse().expect("invalid private key");
+    // Set up providers and user's wallet
+    let provider = Provider::<Http>::try_from(rpc_url.as_str()).expect("Failed to create provider");
+    let private_key = env::var("PRIVATE_KEY_HOLESKY").expect("PRIVATE_KEY must be set");
+    let wallet: LocalWallet = private_key.parse::<LocalWallet>().expect("invalid private key").with_chain_id(chain_id) ;
 
-    // let client = Arc::new(SignerMiddleware::new(provider.clone(), wallet.clone()));
+    let client = Arc::new(SignerMiddleware::new(provider.clone(), wallet.clone()));
 
-    // let fibonachi_verifier_contract_address: Address = "0xBB5F54fB2268152A4A4316D306828B9eC6C0bBbD"
-    //     .parse()
-    //     .unwrap();
+    let fibonachi_verifier_contract_address: Address = "0xBB5F54fB2268152A4A4316D306828B9eC6C0bBbD"
+        .parse()
+        .unwrap();
 
-    // let fibonachi_verifier_contract = FibonacciVerifier::new(fibonachi_verifier_contract_address, client.clone());
+    let fibonachi_verifier_contract = FibonacciVerifier::new(fibonachi_verifier_contract_address, client.clone());
 
     // Try using import generating receipt function from another folder
     let receipt = generating_proof().expect("Failed to generating receipt"); // .expect("Failed to generate proof data");   
-    // print_receipt_properties(&receipt) ;
-    // let journal = receipt.clone().journal.bytes ; 
-    // let seal = encode_seal(&receipt);
+
+    let journal = receipt.clone().journal.bytes ; 
+    let seal = receipt.clone().claim().unwrap().digest().as_bytes().to_vec();
+    let call_data = fibonachi_verifier_contract.verify_and_finalize_fibonachi(seal.into(), journal.into()) ; 
     // println!("Journal : {:?}",journal) ;
     // println!("Seal : {:?}",seal) ;
+    println!("Call data : {:?}",call_data.calldata().unwrap()) ;
 
-    // fibonachi_verifier_contract.verify_and_finalize_fibonachi(seal.into(), journal.into()).await.expect("Failed to verify and finalize fibonachi") ; 
-    // println!("Fibonachi verified and finalized on chain") ;
+    let transaction = TransactionRequest::new()
+        .to(fibonachi_verifier_contract_address)
+        .data(call_data.calldata().unwrap())
+        .gas(300_000u64)
+        .chain_id(chain_id);
+
+    let pending_tx = client.send_transaction(transaction, None).await.expect("Failed to send transaction") ;
+
+    let tx_hash = pending_tx.tx_hash() ; 
     
+    println!("Transaction hash: {:?}", tx_hash);
 }
