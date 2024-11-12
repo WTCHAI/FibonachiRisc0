28a7593cdaf65d8b4d1c36c5699747688e706d2b
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-11-13 01:37:57 +0700

    refactor verifier contract  scheme

--- hardhat/contracts/FibonachiVerifier.sol
+++ hardhat/contracts/FibonachiVerifier.sol
@@ -1,70 +1,33 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.27;
+
+pragma solidity ^0.8.20;
 
 import { IRiscZeroVerifier } from "./Risczero/IRisczeroVerifier.sol";
 import { ImageID } from "./ImageID.sol";
 
 contract FibonachiVerifier {
-    // copy from risczero govenor 
     IRiscZeroVerifier public immutable verifier;
-    bytes32 public immutable imageId = ImageID.FINALIZE_FIBONACHI_ID ;
+    bytes32 public constant imageId = ImageID.FINALIZE_FIBONACHI_ID;
+    uint256 public correct_public_output ;
 
-    uint256 private fibonachiResult ; 
-    bytes32 private journalDigest ; 
-    bytes private seal ; 
+    mapping (address => bool ) IsProverCorrect ;
 
-    event ProofSubmittedLogged(
-        address indexed prover,
-        uint256 timestamp,
-        bool status
-    );
-    event CallingVerifier(address indexed prover);
-
-    constructor(address verifierAddress ){
-        verifier = IRiscZeroVerifier(verifierAddress);
+    constructor(IRiscZeroVerifier _verifier , uint256 correct_public_output_) {
+        verifier = _verifier ;
+        correct_public_output = correct_public_output_ ; 
     }
 
-    function verifyAndFinalizeFibonachi(
-        bytes calldata seal_,
-        bytes calldata journal
-    ) public {
-        emit CallingVerifier(msg.sender);
-        // journal digested 
-        require(journal.length != 0, "Journal data not valid bytes"); 
-
-        journalDigest = sha256(journal) ;
-        fibonachiResult = (
-            (uint64(uint8(journal[0])) << 56) |
-            (uint64(uint8(journal[1])) << 48) |
-            (uint64(uint8(journal[2])) << 40) |
-            (uint64(uint8(journal[3])) << 32) |
-            (uint64(uint8(journal[4])) << 24) |
-            (uint64(uint8(journal[5])) << 16) |
-            (uint64(uint8(journal[6])) << 8) |
-            uint64(uint8(journal[7]))
-        );        
-        seal = seal_ ; 
-
-        // verify the proof
-        try verifier.verify(seal, imageId , journalDigest) { 
-            // If verification is successful, update fibonachiResult and emit success event
-            emit ProofSubmittedLogged(msg.sender, block.timestamp, true);
-        }
-        catch {
-            // If verification fails, emit the failed event with the reason
-            emit ProofSubmittedLogged(msg.sender, block.timestamp,false);
-        }
+    function challenge(address challengerAddr , uint256 fibo_result , bytes memory seal) public {
+        bytes memory journal = abi.encode(fibo_result);
+        require(fibo_result == correct_public_output,"Public output mismatch!") ; 
+        try verifier.verify(seal, imageId, sha256(journal)) {
+            IsProverCorrect[challengerAddr] = true;
+        } catch {
+            IsProverCorrect[challengerAddr] = false;
+        }   
     }
 
-    function getFinalizeFibonachiResult() public view returns(uint256){
-        return fibonachiResult ;
-    }
-
-    function getJournalDigested() public view returns(bytes32){
-        return journalDigest ; 
-    }
-
-    function getSealDigested() public view returns(bytes memory) { 
-        return seal ; 
+    function challengeStatus(address challenger) public view returns (bool) {
+        return IsProverCorrect[challenger] ; 
     }
 }
--- hardhat/scripts/deployFibonachiVerifier.ts
+++ hardhat/scripts/deployFibonachiVerifier.ts
@@ -1,5 +1,6 @@
 
 import hre from "hardhat";
+import { ethers } from "hardhat";
 
 async function Deployer(contractName:string){
     try{
@@ -7,30 +8,21 @@ async function Deployer(contractName:string){
         const ContractFactory = await hre.ethers.getContractFactory(contractName);
         
         const risczeroVerifierContractAddressHole = "0xf70aBAb028Eb6F4100A24B203E113D94E87DE93C" ; 
-        const risczeroVerifierContractAddressSepolia = "0x925d8331ddc0a1F0d96E68CF073DFE1d92b69187" ; 
-        // Getting list of accounts avaliable from hardhat 
-        // Deployer in case didn't set the account private key in hardhat config & env
-        // const deployers = await hre.ethers.getSigners() ; 
-        // console.log("Deploying contract with the account : ",deployers)
-
-        // Deploying the contract using the deployer's signer [0] related on hardhat config
-        // Which contract is require owner so we have to connect owner = msg.sender  
-        // Refactor deploying without telling who is signers cause already declare in env & hardhat config
-        const FibonachiContract = await ContractFactory.deploy(risczeroVerifierContractAddressHole)
+        const FibonachiContract = await ContractFactory.deploy(risczeroVerifierContractAddressHole,825)
         await FibonachiContract.deploymentTransaction()?.wait(3)
 
-        // Verifing contract 
         const FibonachiContractAddress = await FibonachiContract.getAddress();
-        console.log("Contract deployed to : ",await FibonachiContract.getAddress());
+        
         try {
             await hre.run("verify:verify", {
                 address : FibonachiContractAddress,
-                constructorArguments : [risczeroVerifierContractAddressHole]
+                constructorArguments : [risczeroVerifierContractAddressHole,825]
             })
         }catch(err){
             console.error("Error during verification:",err);
             process.exit(1);
         }
+        console.log("Contract deployed to : ",await FibonachiContract.getAddress());
     }catch(err){
         console.error("Error during deployment:", err);
         process.exit(1);
@@ -41,7 +33,10 @@ async function Deployer(contractName:string){
 // running script 
 // npx hardhat run scripts/deploy.ts --network holesky
 // npx hardhat run scripts/deployFibonachiVerifier.ts --network holesky
-// contract address : 0x1E0122e128b316381E439e3aAcDD1aE88E7669F7
+// contract address : 0x206ADBa7b67FF84519Add28bDa6b8b3fE14bd6CA
 
-// 0x29A4E3723f249317A4Ed0d01739D23ADB4B4DaE7
 Deployer("FibonachiVerifier");
+
+// Journal (Hex): 0xbe23000000000000
+// Seal (Hex): 0x50bd176902ec380bfe4b997d3e927880a1f16ee2266ec67c0064f5c6f5fb1458248c588e2981d51d8019a7f6b9ffbd0b538190dba950c09330b10af7b2b15ac090b4b4182875e3a8e39f158b566290510a09fc29b5c079e6204c58464ca0370b630ac2f017cefaf0f9e0129a84bddecc819dc6a808588a03b4dc02efed91c1383add219b2b7dfc79c0aa21748f08d815074ad09db7908e37686d208c846be94ab2abfe952b2839ec925b32c932ff17fd7ef966077e81b7b73c2a758a24a0e875e1b94f9326ff6e0ff1a93eb46d5ec0599104eab7ccd1eef49ea100f409744d3216e94d3b0c3cac33787da061f6a962f4458a4531ed6688c48be3ec4b01ceee6291b6a219
+// payload : JournalPayload { challenger: 0x41649a1f8b2499e2f7884184d062639cef9d0601, journal: [190, 35, 0, 0, 0, 0, 0, 0] }
